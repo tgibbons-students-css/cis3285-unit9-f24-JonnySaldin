c924237eb8848a057133755e0fb840e7c8b82111
Jonny Saldin <jsaldin@css.edu>
2024-10-31 10:53:27 -0500

    Learning activity 9 code push

--- SingleResponsibilityPrinciple/RestfulTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/RestfulTradeDataProvider.cs
@@ -1,21 +1,56 @@
 ﻿using SingleResponsibilityPrinciple.Contracts;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SingleResponsibilityPrinciple
 {
     public class RestfulTradeDataProvider : ITradeDataProvider
     {
+        private readonly string _url;
+        private readonly ILogger _logger;
+
         public RestfulTradeDataProvider(string url, ILogger logger)
         {
+            _url = url;
+            _logger = logger;
         }
 
         public IEnumerable<string> GetTradeData()
         {
-            throw new NotImplementedException();
+            return GetTradeDataAsync().Result; 
+        }
+
+        private async Task<IEnumerable<string>> GetTradeDataAsync()
+        {
+            List<string> tradeData = new List<string>();
+            _logger.LogInfo("Fetching trade data from URL: " + _url);
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(_url);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        _logger.LogInfo($"Failed to fetch trades. Status Code: {response.StatusCode}");
+                        throw new Exception("Could not retrieve trade data from the URL.");
+                    }
+
+                    string jsonData = await response.Content.ReadAsStringAsync();
+                    tradeData = JsonSerializer.Deserialize<List<string>>(jsonData);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInfo("An error occurred while fetching trade data: " + ex.Message);
+                throw;
+            }
+
+            return tradeData;
         }
     }
 }
--- SingleResponsibilityPrinciple/URLTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -1,21 +1,51 @@
 ﻿using SingleResponsibilityPrinciple.Contracts;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace SingleResponsibilityPrinciple
 {
     public class URLTradeDataProvider : ITradeDataProvider
     {
+        private readonly string _url;
+        private readonly ILogger _logger;
+
         public URLTradeDataProvider(string url, ILogger logger)
         {
+            _url = url;
+            _logger = logger;
         }
 
         public IEnumerable<string> GetTradeData()
         {
-            throw new NotImplementedException();
+            List<string> tradeData = new List<string>();
+
+            _logger.LogInfo("Reading trades from URL: " + _url);
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(_url).Result;
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    _logger.LogInfo($"Failed to fetch trades. Status Code: {response.StatusCode}");
+                    throw new Exception("Could not retrieve trade data from the URL.");
+                }
+
+                using (Stream stream = response.Content.ReadAsStreamAsync().Result)
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        tradeData.Add(line);
+                    }
+                }
+            }
+
+            return tradeData;
         }
     }
 }
